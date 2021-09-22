9621c3d78bdff52104ac5613bcd34a15eb4d6649
ChangjiangChen <changjiang0018@126.com>
2021-09-22 22:28:05 +0800

    add simple insertion sort.

---
+++ AlgorithmExperiment/src/main/java/com/changjiang/sort/InsertionSort.java
@@ -0,0 +1,36 @@
+package com.changjiang.sort;
+
+import com.changjiang.sort.util.Common;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    /**
+     * 从第一个元素开始，与前面的元素进行比较
+     * 如果当前元素大于上一个元素，那么比较停止，进入下一轮
+     * 如果当前元素小于上一个元素，那么交换该元素与上一个元素，继续与之前的比较，一直到array[0]
+     *
+     * @param array
+     */
+    public static void sort(int[] array) {
+        if (array == null || array.length < 2) {
+            return;
+        }
+
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i; j >= 1; j--) {
+                if (array[j] >= array[j - 1]) {
+                    break;
+                }
+                Common.swampValue(array, j, j - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {2, 3, 1, 4, 2, 44, 32, 13, 12, 56, 32, 12, 33, 21, 15, 16, 76, 45, 32, 33, 121, 123, 123, 122};
+        sort(array);
+        System.out.println("Arrays.toString(array) = " + Arrays.toString(array));
+    }
+}
